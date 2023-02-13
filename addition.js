4148663be4346515d2ed69cb3b07c22dc6554360
Savannah Samuels <savannahvermeulen10@gmail.com>
2023-02-13 16:26:56 +0200

    API Structure

---
+++ addition.js
@@ -0,0 +1,9 @@
+//step 1
+function addition(a, b) {
+  console.log(`Sum:${a + b}`);
+}
+
+//step 2
+module.exports = {
+  addition,
+};
---
+++ index.js
@@ -0,0 +1,34 @@
+
+// const { addition } = require('./addition.js');
+
+//step 3
+// let { addition } = require('./addition.js');
+// addition(3, 4);
+// addition(8, 9);
+
+// 
+
+// const express = require ('express');
+
+// //Port
+// const port = parseInt(process.env.port) ||4000; 
+
+// //Express app
+// const app = express();
+
+// //Router
+// const route = express.Router();
+
+// app.use(
+//     route
+// )
+
+// //Home or /
+// route.get('/',(req,res)=>{
+//     res.status(200).send('luv you !');
+// })
+
+// app.listen(port,()=>{
+//     console.log(`server is running at ${port}`);
+// })
+
