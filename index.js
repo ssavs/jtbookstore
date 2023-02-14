a4dca71c670e2a868a5bb3b72bf468a0e7a4b671
Savannah Samuels <savannahvermeulen10@gmail.com>
2023-02-14 16:50:22 +0200

    API Structure

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 // const { addition } = require('./addition.js');
 
 //step 3
@@ -6,29 +5,71 @@
 // addition(3, 4);
 // addition(8, 9);
 
-// 
+//
 
-// const express = require ('express');
+const express = require("express");
 
-// //Port
-// const port = parseInt(process.env.port) ||4000; 
+//Path
+const path = require("path");
 
-// //Express app
-// const app = express();
+//db
+const db = require("./config"); //when you want to import use './'
 
-// //Router
-// const route = express.Router();
+// body-parser
+const bodyParser = require("body-parser"); // body-parser - allow us to convert whatever format that is coming from the user data from one format to another
 
-// app.use(
-//     route
-// )
+//Port
+const port = parseInt(process.env.port) || 4000;
 
-// //Home or /
-// route.get('/',(req,res)=>{
-//     res.status(200).send('luv you !');
-// })
+//Express app
+const app = express();
 
-// app.listen(port,()=>{
-//     console.log(`server is running at ${port}`);
-// })
+//Router
+const route = express.Router();
 
+app.use(
+  route,
+  express.json,
+  bodyParser.urlencoded({extended:false}),
+  );
+
+//Home or /
+route.get("/", (req, res) => {
+  res.status(200).sendFile(path.join(__dirname,'./view/index.html'));
+})
+
+//retrieve all users
+route.get('/users',(req,res)=>{
+  const strQry =
+  `
+  SELECT firstName,lastName,emailAddy,country
+  FROM Users;
+  `;
+
+  //db 
+  db.query(strQry, (err,data)=>{
+    if(err) console.log(err);;
+    res.status(200).json({result:data});
+  })
+});
+
+//register
+route.post('/register', bodyParser.json(), (req, res)=> {
+let detail = req.body;
+console.log(detail);
+//sql query
+const strQry = 
+`INSERT INTO Users
+SET?;`;
+db.query(strQry,[detail],(err)=>{
+  if (err) {
+    res.status(400).json({err});
+      } else{
+        res.status(200).json({msg:'A user record was saved.'})
+      }
+})
+})
+
+app.listen(port, () => {
+  console.log(`server is running at ${port}`);
+});
